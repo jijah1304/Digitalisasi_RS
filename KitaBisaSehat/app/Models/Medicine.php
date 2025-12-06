d665c8036895d0553efe756f524bd1a04262c314
jijah1304 <nurulazizzaah@gmail.com>
2025-12-07 01:21:32 +0800

    feat: Medicine Kedaluwarsa #2

--- KitaBisaSehat/app/Models/Medicine.php
+++ KitaBisaSehat/app/Models/Medicine.php
@@ -15,6 +15,7 @@ class Medicine extends Model
         'type',     // 'keras', 'biasa'
         'stock',
         'image',
+        'expiry_date',
     ];
 
     // Obat bisa ada di banyak Rekam Medis (Resep)
@@ -23,5 +24,14 @@ class Medicine extends Model
         return $this->belongsToMany(MedicalRecord::class, 'medical_record_medicine')
                     ->withPivot('quantity')
                     ->withTimestamps();
+    }
+
+    // Method untuk mendapatkan obat yang perlu notifikasi
+    public static function getMedicinesNeedingNotification()
+    {
+        return self::where(function ($query) {
+            $query->where('stock', '<=', 20)
+                  ->orWhere('expiry_date', '<=', now()->addDays(30));
+        })->get();
     }
 }
